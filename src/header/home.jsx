3398722d2840dd971a37e6cbba553f5d654bf817
Kandwanahonathan <nathankandwanaho3@gmail.com>
2025-10-27 16:47:39 +0200

    once

---
+++ src/header/home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Home() {
+    return(
+        <div className="bg-dark">
+
+            <div>
+                <img src="" alt="" />
+            </div>
+
+            <div className="introduction">
+
+                <h1>Your Welcome to our system</h1>
+            </div>
+
+        </div>
+    )
+}
+export default Home
