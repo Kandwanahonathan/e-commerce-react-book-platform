f25387cf26b6d5411637f0e4b2aa415144285564
Kandwanahonathan <nathankandwanaho3@gmail.com>
2025-10-28 20:10:01 +0200

    designing home

--- src/App.jsx
+++ src/App.jsx
@@ -11,16 +11,17 @@ function App() {
     <>
     
     <BrowserRouter>
-    <nav className="navbar bg-primary navbar-dark navbar-expand-lg px-4">
-      <div className='navbar-nav'>
+    <nav className="navbar bg-success navbar-dark navbar-expand-lg px-8">
+      <a className="navbar-brand fw-bold text-warning" href="#">BookStore</a>
+      <div className='navbar-nav ms-auto'>
       <NavLink to='/home' className="nav-link text-white mx-3">Home</NavLink>
-      <NavLink to='/book' className="nav-link text-white mx-3"> Book-visit</NavLink>
+      <NavLink to='/book' className="nav-link text-white mx-3"> Book-Visit</NavLink>
       </div>
     </nav>
 
     <Routes>
       <Route path="/home"  element={<Home/>}/>
-      <Route path='/Book-visit' element={<Visit/>}/>
+      <Route path="/book" element={<Visit/>}/>
     </Routes>
     </BrowserRouter>
     </>
--- src/header/home.jsx
+++ src/header/home.jsx
@@ -1,24 +1,43 @@
-import React, { useState } from "react";
-const[words, setWords ] = useState("You can buy the book","Magic book","Request the book","Your are highly welcome to our site","Don't worry there are the book that can change your life")
+import React,{useState,useEffect} from "react";
+import '../assets/my-app/src/assets/bg of book.jpg'
+
 function Home() {
-    function changeAbleWords(){
-        const word=(()=>{
-            setTimeout()
-        })
-    }
-    return(
-        <div className="">
+    const [promo,setPromo]=useState(["20% off on all fantasy books!",
+  "New arrivals: Bestselling novels this month",
+  "Free shipping on orders above $50"]);
+  const [futureBook,setfutureBook] =useState([
+  { title: "Magic of Thinking Big", author: "David Schwartz", price: 12.99 },
+  { title: "Atomic Habits", author: "James Clear", price: 15.99 },
+  { title: "The Alchemist", author: "Paulo Coelho", price: 10.99 },]);
+  const[testimon, setTestimon]=useState([
+      "This bookstore changed my life!",
+  "Amazing selection and fast delivery!",
+  "Highly recommend for book lovers!"
+  ]);
+  const [alert,setAlert]=useState([
+     "We now deliver internationally!",
+  "New reading club starting next week!",
+  "Subscribe for exclusive offers!"
+  ]);
 
-            <div>
-                <img src="" alt="" />
-            </div>
+return(
+    <div className="container-fluid">
+        <img src="bg of book" alt=" fine bckground " className="img-fluid"/>
 
-            <div className="introduction">
-
-                <h1>Your Welcome to our system</h1>
-            </div>
-
+        <div>
+            <h1>promotion to you</h1>
+            
+                {
+                    promo.map((item,index)=>(
+                        <p key={index}>{item}</p>
+                    ))
+                }
+            
+          
         </div>
-    )
+
+    </div>
+)
+
 }
 export default Home
